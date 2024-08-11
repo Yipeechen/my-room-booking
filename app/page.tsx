c9cbf34c5ea53858f5d487b715743f43e197cfa1
Yipeechen <yipeechen0418@gmail.com>
2024-08-11 18:20:05 +0800

    chore: add mockdata

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,32 @@
 import RoomAllocation from '../components/RoomAllocation'
 
 export default function Home() {
+
+  // TODO: CASE 1: fail
+  const guest = { adult: 4, child: 2 }
+  const rooms = [
+    { roomPrice: 1000, adultPrice: 200, childPrice: 100, capacity: 4 },
+    { roomPrice: 0, adultPrice: 500, childPrice: 500, capacity: 4 },
+    { roomPrice: 500, adultPrice: 300, childPrice: 200, capacity: 4 },
+  ]
+
+  // CASE 2: success
+  // const guest = { adult: 16, child: 0 }
+  // const rooms = [
+  // { roomPrice: 500, adultPrice: 500, childPrice: 300, capacity: 4 },
+  // { roomPrice: 500, adultPrice: 500, childPrice: 300, capacity: 4 },
+  // { roomPrice: 0, adultPrice: 500, childPrice: 300, capacity: 8 },
+  // { roomPrice: 500, adultPrice: 1000, childPrice: 600, capacity: 2 }
+  // ]
+
+  // CASE 3: success
+  // const guest = { adult: 0, child: 1 }
+  // const rooms = [
+  //   { roomPrice: 1000, adultPrice: 500, childPrice: 300, capacity: 2 },
+  //   { roomPrice: 500, adultPrice: 400, childPrice: 300, capacity: 4 },
+  //   { roomPrice: 0, adultPrice: 500, childPrice: 300, capacity: 8 },
+  // ]
+
   return (
     <main>
       <div className='text-left max-w-md mx-auto'>
